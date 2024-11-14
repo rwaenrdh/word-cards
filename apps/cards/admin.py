3752ae4767ebc79c44eeab32d34b15cf91bf5f5a
rwaenrd <963852zxc123@gmail.com>
2024-11-14 11:09:18 +0300

    Add Card, Categories admins

--- apps/cards/admin.py
+++ apps/cards/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from apps.cards.models import Card, Category
 
-admin.site.register(Card)
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['created_by', 'name', ]
+
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ['created_by', 'word', 'translation', ]
